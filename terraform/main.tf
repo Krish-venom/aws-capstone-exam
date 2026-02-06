0e7543a0dd10eef61abeea9851b785c0cd2ba996
Krishna <145960177+Krish-venom@users.noreply.github.com>
2026-02-06 11:03:29 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,12 +113,21 @@ data "aws_availability_zones" "available" {
 }
 
 # Ubuntu 22.04 LTS AMI (Canonical)
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
-  filter { name = "name"                values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] }
-  filter { name = "virtualization-type" values = ["hvm"] }
-}
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
 
 ############################################
 # Network: VPC, Subnets, Routes
