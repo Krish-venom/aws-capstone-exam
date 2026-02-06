03a30c24cb15057a0c83f7e82e12e2ca09b6c0ce
Krishna <145960177+Krish-venom@users.noreply.github.com>
2026-02-06 11:19:35 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,11 +98,21 @@ resource "aws_subnet" "private" {
   }
 }
 
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0"  gateway_id = aws_internet_gateway.igw.id }
-  tags = { Name = "${var.project}-public-rt", Project = var.project }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name    = "${var.project}-public-rt"
+    Project = var.project
+  }
 }
+
 
 resource "aws_route_table_association" "public_assoc" {
   count          = 2
